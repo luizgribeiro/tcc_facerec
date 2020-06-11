b0d2dd1e6972dc4df0d577f2168169e1bd1b1c64
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2020-06-11 18:16:32 -0300

    models added

--- src/models/atendence.py
+++ src/models/atendence.py
@@ -1,6 +1,10 @@
 from pymongo.write_concern import WriteConcern
 from pymodm import EmbeddedMongoModel, MongoModel, fields
+from .students import Student
 
 class Atendence(MongoModel):
     student_id = fields.ReferenceField(Student)
     date = fields.DateTimeField(verbose_name="data da presenca", mongo_name="data_presenca")
+
+    class Meta:
+        collection_name = 'atendence'
--- src/models/students.py
+++ src/models/students.py
@@ -2,7 +2,11 @@ from pymongo.write_concern import WriteConcern
 from pymodm import MongoModel, fields
 
 class Student(MongoModel):
-    student_id = fields.IntegerField(primary_key=True, verbose_name="matricula", mongo_name="matricula")
+    student_id = fields.IntegerField(primary_key=True, verbose_name="matricula", mongo_name="_id")
     email = fields.EmailField(verbose_name="email", mongo_name="email")
     full_name = fields.CharField(verbose_name="nome completo", mongo_name="nome")
-    face_encondings = fields.ListField(verbose_name="face encodings", mongo_name="face_encodings")
+    face_encodings = fields.ListField(verbose_name="face encodings", mongo_name="face_encodings")
+    #TODO: add creation date
+
+    class Meta:
+        collection_name = 'student'
