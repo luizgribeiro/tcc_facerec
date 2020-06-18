992d1d1667612f91bd3f0e4ac2ec9f60b3dbf2ba
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2020-06-18 09:21:46 -0300

    mend

--- src/app.py
+++ src/app.py
@@ -16,25 +16,33 @@ from controllers.face_detector_controller import FaceDetector
 from controllers.face_finder_controller import FaceFinder
 from controllers.students_controller import StudentsController
 from controllers.db_controller import DataBase
+from controllers.face_list_controller import FaceListController
+from controllers.attendance_controller import AttendanceController
 from models.students import Student
+from models.attendance import Attendance
+
+from datetime import datetime
 
 output_frame = None 
 lock = threading.Lock() 
 
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 DataBase('chamada_facial')
 
 
 students_cont = StudentsController(Student)
 student_registry_cont = RegistryController(students_cont)
-face_detector_cont = FaceDetector()
+attendance_cont = AttendanceController(Attendance)
+face_list_cont = FaceListController(app)
+face_detector_cont = FaceDetector(face_list_cont, attendance_cont)
 face_detector_cont.update_known_faces(students_cont)
 face_finder_cont = FaceFinder()
-video = WebcamController(0, face_detector_cont, face_finder_cont)
 
-socketio = SocketIO(app)
+video = WebcamController(face_detector_cont, face_finder_cont, 0)
+
 
 @app.route("/")
 def index():
@@ -44,16 +52,11 @@ def index():
 def cadastro():
     return render_template("./cadastro.htm")
 
+
 @app.route("/video_feed")
 def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-    #ret, jpg_img = video.gen_jpg_from_frame()
     return Response(video.gen_jpg_from_frame(lock), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
-
-def some_function():
-    socketio.emit('some event', {'data': 42})
 
 @app.route("/cadastra_estudante", methods=['POST'])
 def cadastra_estudante():
@@ -71,14 +74,19 @@ def cadastra_estudante():
         else:
             return "Unable to register"
 
-@socketio.on('message')
-def handle_message(message):
-    print(f'received message: {message}')
+
+@socketio.on('update_atendences')
+def update_atendences():
+    if face_list_cont.has_detected():
+        socketio.emit('updated_list', {'faces': face_list_cont.get_detected_faces()} )
+    else:
+        #TODO implement a clear alternative
+        print('No updates available')
 
 
 
 if __name__ == '__main__':
 
-    app.run()
+    socketio.run(app)
 
 
--- src/models/attendance.py
+++ src/models/attendance.py
@@ -2,9 +2,9 @@ from pymongo.write_concern import WriteConcern
 from pymodm import EmbeddedMongoModel, MongoModel, fields
 from .students import Student
 
-class Atendence(MongoModel):
+class Attendance(MongoModel):
     student_id = fields.ReferenceField(Student)
     date = fields.DateTimeField(verbose_name="data da presenca", mongo_name="data_presenca")
 
     class Meta:
-        collection_name = 'atendence'
+        collection_name = 'attendance'
